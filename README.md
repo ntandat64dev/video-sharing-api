1c55aedac3fefdec463e58c267b013e7d26a7176
ntandat <20130221@st.hcmuaf.edu.vn>
2024-03-05 08:56:53 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# video-sharing-api
+
+An online video sharing REST API written in Java Spring Framework.
---
+++ src/test/java/com/example/videosharingapi/VideoSharingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videosharingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoSharingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
